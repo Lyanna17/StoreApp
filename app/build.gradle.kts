479a47bedc38cc3db7f389ca6d74517531531df9
Abigail Vargas <abivargasargu17@gmail.com>
2025-04-25 16:56:41 -0500

    Update firebase 25/04/2025

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -50,6 +51,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -66,4 +68,7 @@ dependencies {
     //cargar imagenes de internet
     implementation(libs.coil.compose)
     implementation(libs.coil.network.okhttp)
+
+    //firebase
+    implementation(libs.firebase.bom)
 }
